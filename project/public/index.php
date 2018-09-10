cc60ce33cd18490f72748aba6affff6630adcd41
Anatoliy <anatoliy.rus@gmail.com>
2018-09-10 16:14:27 +0300

    Точка входа

---
+++ project/public/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/constants.php';
+include_once ROOT_DIR . 'services/Autoloader.php';
+spl_autoload_register([(new app\services\Autoloader()), 'loadClass']);
+
+use app\models\Car;
+use app\models\Category;
+use app\models\Order;
+use app\models\Product;
+use app\models\User;
+
+
+$order = new Order();
+$car = new Car();
+$category = new Category;
+$product = new Product();
+$user = new User();
