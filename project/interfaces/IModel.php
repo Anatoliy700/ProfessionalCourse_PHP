82efad203876a923462b1a99eda6d2831e44b198
Anatoliy <anatoliy.rus@gmail.com>
2018-09-10 16:09:58 +0300

    Интерфейс для Model

---
+++ project/interfaces/IModel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\interfaces;
+
+
+use app\models\Model;
+
+interface IModel
+{
+  public function getOne(int $id): Model;
+  
+  public function getAll(): array;
+  
+  public function getTableName(): string;
+}
