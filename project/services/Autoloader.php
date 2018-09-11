17b902736960569d8f051eb9a75585e703f0e02e
Anatoliy <anatoliy.rus@gmail.com>
2018-09-11 22:11:44 +0300

    Переписал Autoloader

--- project/services/Autoloader.php
+++ project/services/Autoloader.php
@@ -4,11 +4,16 @@ namespace app\services;
 
 class Autoloader
 {
+  
+  public $expansion = '.php';
+  
   public function loadClass($className) {
 //    $pattern = '/^(app\\\)(.+)/';
-    $pattern = '/^(\w+\\\)(.+)/';
-    $replacement = ROOT_DIR . '\$2.php';
-    $fileClass = preg_replace($pattern, $replacement, $className);
+//    $pattern = '/^(\w+\\\)(.+)/';
+//    $replacement = ROOT_DIR . '\$2.php';
+//    $fileClass = preg_replace($pattern, $replacement, $className);
+    $fileClass = str_replace(['app\\', '\\'], [ROOT_DIR, DS], $className);
+    $fileClass .= $this->expansion;
     if (file_exists($fileClass)) {
       include $fileClass;
     } else {
