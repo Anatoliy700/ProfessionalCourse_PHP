9a5ffe3bc72c175baf81072390de88d2993b0ea6
Anatoliy <anatoliy.rus@gmail.com>
2018-09-14 23:12:43 +0300

    Переписал методы для работы со всеми вариантами запросов

--- project/services/Db.php
+++ project/services/Db.php
@@ -3,14 +3,69 @@
 namespace app\services;
 
 
+use app\trains\TSingleton;
+
+
 class Db
 {
-  public function queryOne($sql) {
-    return true;
+  use TSingleton;
+  
+  private $config = [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'login' => 'root',
+    'password' => '',
+    'database' => 'project_php_2',
+    'charset' => 'utf8'
+  ];
+  
+  private $conn = null;
+  
+  private function getConnection() {
+    if (is_null($this->conn)) {
+      $this->conn = new \PDO(
+        $this->prepareDnsString(),
+        $this->config['login'],
+        $this->config['password']
+      );
+      $this->conn->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+    }
+    return $this->conn;
   }
   
-  public function queryAll() {
-    return true;
+  private function prepareDnsString() {
+    return sprintf("%s:host=%s;dbname=%s;charset=%s",
+      $this->config['driver'],
+      $this->config['host'],
+      $this->config['database'],
+      $this->config['charset']
+    );
+  }
+  
+  private function query($sql, $params = []) {
+    $pdoStatement = $this->getConnection()->prepare($sql);
+    $pdoStatement->execute($params);
+    return $pdoStatement;
+  }
+  
+  public function execute($sql, $params = []) {
+    return $this->query($sql, $params);
+  }
+  
+  public function queryOne($sql, $params = [], $className = null) {
+    return $this->queryAll($sql, $params, $className)[0];
+  }
+  
+  public function queryAll($sql, $params = [], $className = null) {
+    $pdoStatement = $this->query($sql, $params);
+    if (!is_null($className)) {
+      $pdoStatement->setFetchMode(\PDO::FETCH_CLASS, $className);
+    }
+    return $pdoStatement->fetchAll();
+  }
+  
+  public function getLastInsertId() {
+    return $this->getConnection()->lastInsertId();
   }
   
 }
