fb7f0cc7f36cad0b025c576de19011f0ee3b9be9
Anatoliy <anatoliy.rus@gmail.com>
2018-09-17 21:01:41 +0300

    Добавил константы

--- project/config/main.php
+++ project/config/main.php
@@ -1,3 +1,6 @@
 <?php
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'].DS.'..'.DS);
+define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . DS . '..' . DS);
+define("TEMPLATES_DIR", ROOT_DIR . "views" . DS);
+define("DEFAULT_CONTROLLER", "product");
+define("CONTROLLERS_NAMESPACE", "app\\controllers");
