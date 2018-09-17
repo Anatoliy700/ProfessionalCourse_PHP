69a9a5a4f3593c086eb97c73ffd167c79f2b636d
Anatoliy <anatoliy.rus@gmail.com>
2018-09-17 21:18:22 +0300

    Поменялось имя столбца

--- project/structureDB/products.sql
+++ project/structureDB/products.sql
@@ -2,7 +2,7 @@ CREATE TABLE products
 (
   id          INT AUTO_INCREMENT
     PRIMARY KEY,
-  name        VARCHAR(50)  NOT NULL,
+  title        VARCHAR(50)  NOT NULL,
   description VARCHAR(255) NOT NULL,
   price       INT          NOT NULL,
   producer    VARCHAR(125) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE products
 CREATE INDEX product_category_id_fk
   ON products (category_id);
 CREATE INDEX product_name_index
-  ON products (name);
+  ON products (title);
 CREATE INDEX product_price_index
   ON products (price);
 CREATE INDEX product_producer_index
