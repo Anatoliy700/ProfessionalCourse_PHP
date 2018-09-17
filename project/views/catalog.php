1ba0ae345e4a329fd26f9d0da1781624a7bb8914
Anatoliy <anatoliy.rus@gmail.com>
2018-09-17 21:23:48 +0300

    Шаблон каталога

---
+++ project/views/catalog.php
@@ -0,0 +1,16 @@
+
+<h2>Товары</h2>
+<div class="wrap-products">
+  <?php foreach ($products as $product): ?>
+    <div class="product">
+      <a href="?a=card&id=<?= $product->id ?>">
+        <div>
+          <h3><?= $product->title ?></h3>
+          <img src="/img/min/<?= $product->path ?>.jpg" alt="<?= $product->path ?>">
+          <p><?= $product->description ?></p>
+          <p>Стоимость: <?= $product->price ?> руб.</p>
+        </div>
+      </a>
+    </div>
+  <?php endforeach; ?>
+</div>
