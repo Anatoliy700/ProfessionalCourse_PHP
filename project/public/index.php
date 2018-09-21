ccd5d9949eba7b33b64fe9b1a1f956b996e7c026
Anatoliy <anatoliy.rus@gmail.com>
2018-09-21 21:32:21 +0300

    Подключил шаблонизатор twig, autoload через composer

--- project/public/index.php
+++ project/public/index.php
@@ -1,7 +1,8 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
-include_once ROOT_DIR . 'services/Autoloader.php';
-spl_autoload_register([(new app\services\Autoloader()), 'loadClass']);
+require_once ROOT_DIR . '/vendor/autoload.php';
+//include_once ROOT_DIR . 'services/Autoloader.php';
+//spl_autoload_register([(new app\services\Autoloader()), 'loadClass']);
 
 session_start();
 
@@ -10,8 +11,11 @@ $action = $_GET['a'];
 
 $controllerClass = CONTROLLERS_NAMESPACE . "\\" . ucfirst($controllerName) . "Controller";
 
+$loader = new Twig_Loader_Filesystem(TEMPLATES_DIR);
+$loader->addPath(TEMPLATES_DIR . 'layouts/', 'layouts');
+$twig = new Twig_Environment($loader);
 if (class_exists($controllerClass)) {
-  $controller = new $controllerClass;
+  $controller = new $controllerClass($twig);
   $controller->run($action);
 }
 
