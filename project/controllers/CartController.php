6e8727c894de8356fce0ca8923de1eb0163de6a5
Anatoliy <anatoliy.rus@gmail.com>
2018-09-22 19:41:49 +0300

    revert (Поменял template на twig)

--- project/controllers/CartController.php
+++ project/controllers/CartController.php
@@ -18,7 +18,7 @@ class CartController extends Controller
     if ($session->isProducts()) {
       $cart = new Cart($session->cart['products']);
     }
-    echo $this->render('twig_cart.html', ['cart' => $cart]);
+    echo $this->render('cart', ['cart' => $cart]);
   }
   
   /**
