e265aab7eeb401bbe45b1d04a73304ba0920a7d1
Anatoliy <anatoliy.rus@gmail.com>
2018-09-25 15:38:12 +0300

    Создание объекта renderer, добавлена обработка исключений

--- project/public/index.php
+++ project/public/index.php
@@ -1,4 +1,7 @@
 <?php
+
+use \app\services\exception\ControllerException;
+
 include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
 require_once ROOT_DIR . '/vendor/autoload.php';
 //include_once ROOT_DIR . 'services/Autoloader.php';
@@ -7,13 +10,22 @@ require_once ROOT_DIR . '/vendor/autoload.php';
 session_start();
 
 $request = new \app\services\Request();
+$renderer = new \app\services\Renderer(new \app\services\TemplateRenderer());
 
 $controllerName = $request->getControllerName() ?: DEFAULT_CONTROLLER;
 $action = $request->getActionName();
 
 $controllerClass = CONTROLLERS_NAMESPACE . "\\" . ucfirst($controllerName) . "Controller";
-
-if (class_exists($controllerClass)) {
-  $controller = new $controllerClass(new \app\services\TemplateRenderer(), $request);
-  $controller->run($action);
+try {
+  if (class_exists($controllerClass)) {
+    $controller = new $controllerClass($renderer, $request);
+    $controller->run($action);
+  } else {
+    throw new ControllerException('Запрашивемая страница не найдена');
+  }
+} catch (ControllerException $e) {
+  echo $renderer->render('404', ['message' => $e->getMessage()]);
+} catch (Exception | Error $e){
+  echo $renderer->render('404', ['Произошла ошибка']);
+  //отправляем данные ошибки на почту
 }
