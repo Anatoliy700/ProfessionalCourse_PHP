8e16a140f0326dec2b047d21f0376bbd796000a9
Anatoliy <anatoliy.rus@gmail.com>
2018-09-28 18:08:18 +0300

    Вся логика перенесена в App, здесь только получаем конфигурацию и запускаем приложение

--- project/public/index.php
+++ project/public/index.php
@@ -1,31 +1,4 @@
 <?php
-
-use \app\services\exception\ControllerException;
-
-include_once $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
-require_once ROOT_DIR . '/vendor/autoload.php';
-//include_once ROOT_DIR . 'services/Autoloader.php';
-//spl_autoload_register([(new app\services\Autoloader()), 'loadClass']);
-
-session_start();
-
-$request = new \app\services\Request();
-$renderer = new \app\services\Renderer(new \app\services\TemplateRenderer());
-
-$controllerName = $request->getControllerName() ?: DEFAULT_CONTROLLER;
-$action = $request->getActionName();
-
-$controllerClass = CONTROLLERS_NAMESPACE . "\\" . ucfirst($controllerName) . "Controller";
-try {
-  if (class_exists($controllerClass)) {
-    $controller = new $controllerClass($renderer, $request);
-    $controller->run($action);
-  } else {
-    throw new ControllerException('Запрашивемая страница не найдена');
-  }
-} catch (ControllerException $e) {
-  echo $renderer->render('404', ['message' => $e->getMessage()]);
-} catch (Exception | Error $e){
-  echo $renderer->render('404', ['Произошла ошибка']);
-  //отправляем данные ошибки на почту
-}
+include_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+$config = include $_SERVER['DOCUMENT_ROOT'] . '/../config/main.php';
+\app\base\App::call()->run($config);
