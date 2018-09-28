3ff7b0c879846920d8323171f350d531738c21bb
Anatoliy <anatoliy.rus@gmail.com>
2018-09-28 23:28:25 +0300

    Теперь наследует классу для работы с сессиями

--- project/models/repositories/CartRepository.php
+++ project/models/repositories/CartRepository.php
@@ -7,18 +7,8 @@ use app\models\entities\Cart;
 use app\models\entities\DataEntity;
 use app\services\Session;
 
-class CartRepository extends Repository
+class CartRepository extends SessionRepository
 {
-  
-  protected $ss;
-  
-  /**
-   * CartRepository constructor.
-   */
-  public function __construct() {
-    $this->ss = new Session($this->getTableName());
-  }
-  
   
   protected function getTableName(): string {
     return 'carts';
@@ -27,37 +17,4 @@ class CartRepository extends Repository
   protected function getEntityClass() {
     return Cart::class;
   }
-  
-  public function getOne(int $id): DataEntity {
-    return $this->getCartFromSession();
-  }
-  
-  public function getAll(): array {
-  }
-  
-  public function getSelect($arrId): array {
-  }
-  
-  public function delete(DataEntity $entity) {
-    $this->ss->save(null);
-  }
-  
-  public function save(DataEntity $entity) {
-    $this->saveToSession($entity);
-  }
-  
-  /**
-   * @return DataEntity
-   */
-  private function getCartFromSession(): DataEntity {
-    $className = $this->getEntityClass();
-    return new $className($this->ss->getData());
-  }
-  
-  //TODO: сохранять в сессию тока id товара и количество
-  private function saveToSession($entity) {
-    $this->ss->save($entity->toArray());
-  }
-  
-  
 }
