5110e598341bf2aeb0094aee32e1d379337bb4f6
Anatoliy <anatoliy.rus@gmail.com>
2018-09-28 18:05:56 +0300

    Теперь возвращает массив настроек приложения

--- project/config/main.php
+++ project/config/main.php
@@ -1,6 +1,29 @@
 <?php
-define('DS', DIRECTORY_SEPARATOR);
-define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT'] . DS . '..' . DS);
-define("TEMPLATES_DIR", ROOT_DIR . "views" . DS);
-define("DEFAULT_CONTROLLER", "product");
-define("CONTROLLERS_NAMESPACE", "app\\controllers");
+return [
+  'ds' => DIRECTORY_SEPARATOR,
+  'rootDir' => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR,
+  'templatesDir' => __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR,
+  'defaultController' => 'product',
+  'controllersNamespace' => 'app\controllers',
+  'components' => [
+    'db' => [
+      'class' => \app\services\Db::class,
+      'driver' => 'mysql',
+      'host' => 'localhost',
+      'login' => 'root',
+      'password' => '',
+      'database' => 'project_php_2',
+      'charset' => 'utf8'
+    ],
+    'request' => [
+      'class' => \app\services\Request::class
+    ],
+    'templateRenderer' => [
+      'class' => \app\services\TemplateRenderer::class
+    ],
+    'renderer' => [
+      'class' => \app\services\Renderer::class,
+      'use' => \app\services\TemplateRenderer::class
+    ]
+  ]
+];
