dd7dc82408dd8f12dfa7c3cbfda08373fc9a1e1d
Anatoliy <anatoliy.rus@gmail.com>
2018-09-28 18:14:42 +0300

    Обращение к конфигурации теперь через App

--- project/services/TemplateRenderer.php
+++ project/services/TemplateRenderer.php
@@ -3,6 +3,7 @@
 namespace app\services;
 
 
+use app\base\App;
 use app\interfaces\IRenderer;
 
 class TemplateRenderer implements IRenderer
@@ -10,7 +11,7 @@ class TemplateRenderer implements IRenderer
   public function render($template, $params = []) {
     ob_start();
     extract($params);
-    $templatePath = TEMPLATES_DIR . $template . ".php";
+    $templatePath = App::call()->config['templatesDir'] . $template . ".php";
     include $templatePath;
     return ob_get_clean();
   }
