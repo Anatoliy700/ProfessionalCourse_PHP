b2df03f46091dc36ab92989954766cad9523448a
Anatoliy <anatoliy.rus@gmail.com>
2018-09-29 20:54:37 +0300

    Оптимизация, добавил экшен разлогивания

---
+++ project/controllers/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace app\controllers;
+
+
+use app\base\App;
+use app\services\exception\AuthException;
+use app\services\Redirect;
+
+class AuthController extends Controller
+{
+  public function actionIndex() {
+    if (App::call()->authorization->isAuth()) {
+      Redirect::go('/lk');
+    }
+    echo $this->render('auth');
+  }
+  
+  public function actionIn() {
+    $login = $this->request->getParams('login');
+    $pass = $this->request->getParams('pass');
+    if ($this->request->isPost() && $login && $pass) {
+      try {
+        App::call()->authorization->auth($login, $pass);
+      } catch (AuthException $e) {
+        echo $this->render('auth', ['message' => $e->getMessage()]);
+        exit();
+      }
+    }
+    Redirect::go('/auth');
+  }
+  
+  public function actionOut() {
+    App::call()->authorization->out();
+    Redirect::go('/auth');
+  }
+}
