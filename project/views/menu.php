6bb51f9458142179317430ea6919456366ef9eed
Anatoliy <anatoliy.rus@gmail.com>
2018-09-29 21:13:52 +0300

    Шаблон меню

---
+++ project/views/menu.php
@@ -0,0 +1,14 @@
+<?php return [
+  'auth' => '
+    <a href="/product">Каталог</a>&nbsp;&nbsp;
+    <a href="/cart">Корзина</a>&nbsp;&nbsp;
+    <a href="/order">Заказы</a>&nbsp;&nbsp;
+    <a href="/lk">Личный кабинет</a>&nbsp;&nbsp;
+    <a href="/auth/out">Выход</a>&nbsp;&nbsp;
+',
+  'noAuth' => '
+    <a href="/product">Каталог</a>&nbsp;&nbsp;
+    <a href="/lk">Личный кабинет</a>&nbsp;&nbsp;
+    <a href="/auth">Авторизоваться</a>&nbsp;&nbsp;
+    <a href="/registration">Зарегистрироваться</a>&nbsp;&nbsp;
+'];
