88efea7b5ab217cd0a7fa8d2253ea740e64b9ee6
Anatoliy <anatoliy.rus@gmail.com>
2018-09-29 21:01:16 +0300

    Переопределение методов

--- project/models/repositories/CartRepository.php
+++ project/models/repositories/CartRepository.php
@@ -5,7 +5,6 @@ namespace app\models\repositories;
 
 use app\models\entities\Cart;
 use app\models\entities\DataEntity;
-use app\services\Session;
 
 class CartRepository extends SessionRepository
 {
@@ -16,5 +15,28 @@ class CartRepository extends SessionRepository
   
   protected function getEntityClass() {
     return Cart::class;
+  }
+  
+  
+  /**
+   * @param int|null $id
+   * @return DataEntity
+   */
+  public function getOne(int $id = null): DataEntity {
+    $className = $this->getEntityClass();
+    $reflectionClass = new \ReflectionClass($className);
+    return $reflectionClass->newInstanceArgs($this->get());
+
+//    return new $className($this->ss->getData());
+  }
+  
+  
+  public function delete(DataEntity $entity) {
+    $this->set();
+  }
+  
+  //TODO: сохранять в сессию тока id товара и количество
+  public function save(DataEntity $entity) {
+    $this->set($entity);
   }
 }
