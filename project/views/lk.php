d9724f77f373330317ca2c5ed527da7d86f162a5
Anatoliy <anatoliy.rus@gmail.com>
2018-09-29 21:13:37 +0300

    Шаблон личного кабинета

---
+++ project/views/lk.php
@@ -0,0 +1,7 @@
+<div class="wrap-lk">
+  <h2>Личный кабинет</h2>
+  <p><b><?= $user->getFullName() ?></b>, добро пожаловать в ваш личный кабинет!</p>
+  <p>Ваш логин в сервисе: <b><?= $user->getLogin() ?></b></p>
+  <div class="wrap_orders-lk">
+  </div>
+</div>
