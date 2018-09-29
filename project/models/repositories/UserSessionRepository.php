4923ba682cf226264efb2046a0ffd643bd65bf4e
Anatoliy <anatoliy.rus@gmail.com>
2018-09-29 21:06:54 +0300

    Репозиторий для сохранения и получения User из сессии

--- project/models/repositories/UserSessionRepository.php
+++ project/models/repositories/UserSessionRepository.php
@@ -3,19 +3,17 @@
 namespace app\models\repositories;
 
 
+use app\models\entities\DataEntity;
 use app\models\entities\User;
 
 class UserSessionRepository extends SessionRepository
 {
-  protected function getTableName(): string {
-    return 'user';
-  }
-  
   protected function getEntityClass() {
     return User::class;
   }
   
-  public function isAuth(){
-    return (bool)$this->get();
+  public function getUser(): DataEntity {
+    $reflectionClass = new \ReflectionClass($this->getEntityClass());
+    return $reflectionClass->newInstanceArgs($this->get());
   }
 }
