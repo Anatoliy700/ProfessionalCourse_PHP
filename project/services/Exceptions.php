033a9effae3e19d63a528e8b4f40c044dee52bf4
Anatoliy <anatoliy.rus@gmail.com>
2018-09-29 21:08:14 +0300

    Д обявлено свойство для хранения данных

--- project/services/Exceptions.php
+++ project/services/Exceptions.php
@@ -1,10 +1,28 @@
 <?php
+
 namespace app\services\exception;
 
 class RepositoryException extends \Exception
 {
+  protected $param;
+  
+  /**
+   * RepositoryException constructor.
+   */
+  public function __construct($message, $code, $param) {
+    parent::__construct($message, $code);
+    $this->param = $param;
+  }
+  
+  public function getParam() {
+    return $this->param;
+  }
 }
 
 class ControllerException extends \Exception
+{
+}
+
+class AuthException extends \Exception
 {
 }
